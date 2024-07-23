3c369c9acb6a6ff1ceadc01c1caca4d27bb59e74
hashimoto3514 <hashimoto3514@gmail.com>
2024-07-23 23:17:56 +0900

    202407232317

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import nippoListView, nippoDetailView, nippoCreateView
 
 urlpatterns = [
-    
+    path("", nippoListView),
+    path("detail/<int:number>/", nippoDetailView),
+    path("create/", nippoCreateView),
 ]
