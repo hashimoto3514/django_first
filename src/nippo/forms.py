0c0f4fc4752ed4bf35df98e8edba95bf7241fa9b
hashimoto3514 <hashimoto3514@gmail.com>
2024-07-25 23:19:20 +0900

    202407252319

--- src/nippo/forms.py
+++ src/nippo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class NippoFormClass(forms.Form):
-    title = forms.CharField()
-    content = forms.CharField()
+    title = forms.CharField(label="タイトル", widget=forms.TextInput(attrs={'placeholder':'タイトル...'}))
+    content = forms.CharField(label="内容", widget=forms.Textarea(attrs={'placeholder':'内容...'}))
 
